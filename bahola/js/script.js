93c5bf6ece50268d25e487d3ea8e153cbd110cb4
Bukhara Dev <149585436+BUKHARA07@users.noreply.github.com>
2025-12-13 11:23:02 -0700

    Add files via upload

---
+++ bahola/js/script.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", function () {
+
+    // const select = document.querySelector("select");
+    // const link = document.querySelector("#link");
+
+    // select.addEventListener("change", () => {
+    //     if (select.value === "student") {
+    //         link.href = "./pages/student.html";
+    //     } else if (select.value === "teacher") {
+    //         link.href = "./pages/teacher.html";
+    //     }
+    // });
+
+})
